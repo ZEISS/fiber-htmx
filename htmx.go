b7c653928347eef219c3692d251407d8c7384ca4
Sebastian Döll <sebastian@katallaxie.dev>
2024-02-03 21:14:00 +0000

    wip: add helper functions

--- htmx.go
+++ htmx.go
@@ -1,7 +1,15 @@
 package htmx
 
 import (
+	"encoding/json"
+	"html/template"
+
 	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	// StatusStopPolling ...
+	StatusStopPolling = 286
 )
 
 // HxRequestHeader ...
@@ -96,6 +104,83 @@ func New(config ...Config) fiber.Handler {
 		}
 
 		return ContextWithHx(c).Next()
+	}
+}
+
+// Htmx ...
+type Htmx struct {
+	request *Hx
+	ctx     *fiber.Ctx
+}
+
+// IsHxRequest ...
+func (h *Htmx) IsHxRequest() bool {
+	return h.request.HxRequest
+}
+
+// IsHxBoosted ...
+func (h *Htmx) IsHxBoosted() bool {
+	return h.request.HxBoosted
+}
+
+// IsHxHistoryRestoreRequest ...
+func (h *Htmx) IsHxHistoryRestoreRequest() bool {
+	return h.request.HxHistoryRestoreRequest
+}
+
+// RenderPartial ...
+func (h *Htmx) RenderPartial() bool {
+	return (h.request.HxRequest || h.request.HxBoosted) && !h.request.HxHistoryRestoreRequest
+}
+
+// Write ...
+func (h *Htmx) Write(data []byte) (n int, err error) {
+	return h.ctx.Write(data)
+}
+
+// WriteString ...
+func (h *Htmx) WriteString(s string) (n int, err error) {
+	return h.Write([]byte(s))
+}
+
+// StopPolling ...
+func (h *Htmx) StopPolling() error {
+	return h.ctx.SendStatus(StatusStopPolling)
+}
+
+// Ctx ...
+func (h *Htmx) Ctx() *fiber.Ctx {
+	return h.ctx
+}
+
+// WriteJSON ...
+func (h *Htmx) WriteJSON(data any) (n int, err error) {
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return h.Write(payload)
+}
+
+// WriteHTML ...
+func (h *Htmx) WriteHTML(html template.HTML) (n int, err error) {
+	return h.Write([]byte(html))
+}
+
+// HtmxHandler ...
+type HtmxHandlerFunc = func(hx *Htmx) error
+
+// NewHtmxHandler ...
+func NewHtmxHandler(handler HtmxHandlerFunc, config ...Config) fiber.Handler {
+	cfg := configDefault(config...)
+
+	return func(c *fiber.Ctx) error {
+		hx := HxFromContext(c)
+
+		h := &Htmx{hx, c}
+
+		return handler(h)
 	}
 }
 
