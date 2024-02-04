4cc8a9ac0f3420b8cce743a754951278a980ae12
Sebastian Döll <sebastian@katallaxie.dev>
2024-02-04 19:13:50 +0000

    wip: add response header

--- htmx.go
+++ htmx.go
@@ -3,6 +3,7 @@ package htmx
 import (
 	"encoding/json"
 	"html/template"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -19,6 +20,42 @@ type HxRequestHeader string
 func (h HxRequestHeader) String() string {
 	return string(h)
 }
+
+// HxResponseHeader ...
+type HxResponseHeader string
+
+// HxResponseHeaders ...
+type HxResponseHeaders struct {
+	headers http.Header
+}
+
+// String ...
+func (h HxResponseHeader) String() string {
+	return string(h)
+}
+
+// Set ...
+func (h *HxResponseHeaders) Set(k HxResponseHeader, val string) {
+	h.headers.Set(k.String(), val)
+}
+
+func (h *HxResponseHeaders) Get(k HxResponseHeader) string {
+	return h.headers.Get(k.String())
+}
+
+const (
+	HXLocation           HxResponseHeader = "HX-Location"             // Allows you to do a client-side redirect that does not do a full page reload
+	HXPushUrl            HxResponseHeader = "HX-Push-Url"             // pushes a new url into the history stack
+	HXRedirect           HxResponseHeader = "HX-Redirect"             // can be used to do a client-side redirect to a new location
+	HXRefresh            HxResponseHeader = "HX-Refresh"              // if set to "true" the client side will do a full refresh of the page
+	HXReplaceUrl         HxResponseHeader = "HX-Replace-Url"          // replaces the current URL in the location bar
+	HXReswap             HxResponseHeader = "HX-Reswap"               // Allows you to specify how the response will be swapped. See hx-swap for possible values
+	HXRetarget           HxResponseHeader = "HX-Retarget"             // A CSS selector that updates the target of the content update to a different element on the page
+	HXReselect           HxResponseHeader = "HX-Reselect"             // A CSS selector that allows you to choose which part of the response is used to be swapped in. Overrides an existing hx-select on the triggering element
+	HXTrigger            HxResponseHeader = "HX-Trigger"              // allows you to trigger client side events, see the documentation for more info
+	HXTriggerAfterSettle HxResponseHeader = "HX-Trigger-After-Settle" // allows you to trigger client side events, see the documentation for more info
+	HXTriggerAfterSwap   HxResponseHeader = "HX-Trigger-After-Swap"   // allows you to trigger client side events, see the documentation for more info
+)
 
 const (
 	HxRequestHeaderBoosted               HxRequestHeader = "HX-Boosted"
