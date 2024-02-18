2d8ea6c9f56ab085aae72aead6245fea0c1c9c55
Sebastian Döll <sebastian@katallaxie.dev>
2024-02-18 19:58:38 +0000

    chore: add goreleaser tools

--- tools/tools.go
+++ tools/tools.go
@@ -6,6 +6,7 @@ package tools
 import (
 	_ "github.com/golang/mock/mockgen/model"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goreleaser/goreleaser"
 	_ "gotest.tools/gotestsum"
 	_ "mvdan.cc/gofumpt"
 )
