baf2eb2099304762e7a270d658f0f2ff9d2734ff
Sebastian Döll <sebastian@katallaxie.dev>
2024-02-19 20:29:42 +0000

    fix: use children spread

--- components/links/links.go
+++ components/links/links.go
@@ -11,32 +11,31 @@ type LinkProps struct {
 	Rel        string          // The relationship between the current document and the linked document.
 	Href       string          // The URL of the linked document.
 	ClassNames map[string]bool // The class names for the link element.
-	Children   []htmx.Node
 
 	ctx context.Context
 }
 
 // WithContext returns the LinkProps with the provided context.
-func (p LinkProps) WithContext(ctx context.Context) LinkProps {
+func (p LinkProps) WithContext(ctx context.Context, children ...htmx.Node) LinkProps {
 	p.ctx = ctx
 
 	return p
 }
 
 // Link generates a link element based on the provided properties.
-func Link(p LinkProps) htmx.Node {
+func Link(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link": true,
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Primary generates a primary link element based on the provided properties.
-func Primary(p LinkProps) htmx.Node {
+func Primary(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":         true,
@@ -44,12 +43,12 @@ func Primary(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Secondary generates a secondary link element based on the provided properties.
-func Secondary(p LinkProps) htmx.Node {
+func Secondary(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":           true,
@@ -57,12 +56,12 @@ func Secondary(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Accent generates an accent link element based on the provided properties.
-func Accent(p LinkProps) htmx.Node {
+func Accent(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":        true,
@@ -70,12 +69,12 @@ func Accent(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Neutral generates a neutral link element based on the provided properties.
-func Neutral(p LinkProps) htmx.Node {
+func Neutral(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":         true,
@@ -83,12 +82,12 @@ func Neutral(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Info generates an info link element based on the provided properties.
-func Info(p LinkProps) htmx.Node {
+func Info(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":      true,
@@ -96,12 +95,12 @@ func Info(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Warning generates a warning link element based on the provided properties.
-func Warning(p LinkProps) htmx.Node {
+func Warning(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":         true,
@@ -109,12 +108,12 @@ func Warning(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Error generates an error link element based on the provided properties.
-func Error(p LinkProps) htmx.Node {
+func Error(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":       true,
@@ -122,12 +121,12 @@ func Error(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
 
 // Underline generates an underline link element based on the provided properties.
-func Underline(p LinkProps) htmx.Node {
+func Underline(p LinkProps, children ...htmx.Node) htmx.Node {
 	return htmx.A(
 		htmx.ClassNames{
 			"link":       true,
@@ -135,6 +134,6 @@ func Underline(p LinkProps) htmx.Node {
 		}.Merge(p.ClassNames),
 		htmx.Href(p.Href),
 		htmx.Rel(p.Rel),
-		htmx.Group(p.Children...),
+		htmx.Group(children...),
 	)
 }
