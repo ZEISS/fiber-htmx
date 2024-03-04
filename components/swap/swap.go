a84499ae161f16b8bd8237afaefb3b8fd18129b4
Sebastian Döll <sebastian@katallaxie.dev>
2024-03-04 14:54:51 +0000

    feat: add swap component

---
+++ components/swap/swap.go
@@ -0,0 +1,50 @@
+package swap
+
+import htmx "github.com/zeiss/fiber-htmx"
+
+// SwapProps contains the properties for the swap component.
+type SwapProps struct {
+	ClassNames htmx.ClassNames
+
+	htmx.Ctx
+}
+
+// SwapOn is a component for the on state of the swap component.
+func SwapOn(p SwapProps, children ...htmx.Node) htmx.Node {
+	return htmx.Div(
+		htmx.Merge(
+			htmx.ClassNames{
+				"swap-on": true,
+			},
+			p.ClassNames,
+		),
+	)
+}
+
+// SwapOff is a component for the off state of the swap component.
+func SwapOff(p SwapProps, children ...htmx.Node) htmx.Node {
+	return htmx.Div(
+		htmx.Merge(
+			htmx.ClassNames{
+				"swap-off": true,
+			},
+			p.ClassNames,
+		),
+	)
+}
+
+// Swap is a component for the htmx swap extension.
+func Swap(p SwapProps, children ...htmx.Node) htmx.Node {
+	return htmx.Label(
+		htmx.Merge(
+			htmx.ClassNames{
+				"swap": true,
+			},
+			p.ClassNames,
+		),
+		htmx.Input(
+			htmx.Attribute("type", "checkbox"),
+		),
+		htmx.Group(children...),
+	)
+}
