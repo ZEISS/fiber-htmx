309e13beec0948d03f3fbac6ece8cf84edf9bb31
Sebastian Döll <sebastian@katallaxie.dev>
2024-03-19 20:24:56 +0000

    feat: add color names

---
+++ components/colors/colors.go
@@ -0,0 +1,26 @@
+package colors
+
+type ColorName string // ColorName is a type for color names.
+
+const (
+	Primary          ColorName = "bg-primary"
+	PrimaryContent   ColorName = "bg-primary-content"
+	Secondary        ColorName = "bg-secondary"
+	SecondaryContent ColorName = "bg-secondary-content"
+	Accent           ColorName = "bg-accent"
+	AccentContent    ColorName = "bg-accent-content"
+	Neutral          ColorName = "bg-neutral"
+	NeutralContent   ColorName = "bg-neutral-content"
+	Base100          ColorName = "bg-base-100"
+	Base200          ColorName = "bg-base-200"
+	Base300          ColorName = "bg-base-300"
+	BaseContent      ColorName = "bg-base-content"
+	Success          ColorName = "bg-success"
+	SuccessContent   ColorName = "bg-success-content"
+	Error            ColorName = "bg-error"
+	ErrorContent     ColorName = "bg-error-content"
+	Warning          ColorName = "bg-warning"
+	WarningContent   ColorName = "bg-warning-content"
+	Info             ColorName = "bg-info"
+	InfoContent      ColorName = "bg-info-content"
+)
