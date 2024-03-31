ca3e7c998188bc885a799859eafe3ded62876826
Sebastian Döll <sebastian@katallaxie.dev>
2024-03-31 19:13:09 +0000

    feat: adding form control

--- components/forms/checkbox.go
+++ components/forms/checkbox.go
@@ -2,6 +2,48 @@ package forms
 
 import htmx "github.com/zeiss/fiber-htmx"
 
+// FormControlProps represents the properties for a form control element.
+type FormControlProps struct {
+	ClassNames htmx.ClassNames // The class names for the form control element.
+}
+
+// FormControl generates a form control element based on the provided properties.
+func FormControl(p FormControlProps, children ...htmx.Node) htmx.Node {
+	return htmx.Div(
+		htmx.Merge(
+			htmx.ClassNames{
+				"form-control": true,
+			},
+			p.ClassNames,
+		),
+		htmx.Label(
+			htmx.ClassNames{
+				"label":          true,
+				"cursor-pointer": true,
+			},
+			htmx.Group(children...),
+		),
+	)
+}
+
+// FormControlLabelProps represents the properties for a form control label element.
+type FormControlLabelTextProps struct {
+	ClassNames htmx.ClassNames // The class names for the form control label element.
+}
+
+// FormControlLabel generates a form control label element based on the provided properties.
+func FormControlLabelText(p FormControlLabelTextProps, children ...htmx.Node) htmx.Node {
+	return htmx.Span(
+		htmx.Merge(
+			htmx.ClassNames{
+				"label-text": true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
 // CheckboxProps represents the properties for a checkbox element.
 type CheckboxProps struct {
 	ClassNames htmx.ClassNames // The class names for the checkbox element.
