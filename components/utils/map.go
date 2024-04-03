f22d03838ec1e6b7939a010285ed82f8595e0ada
Sebastian Döll <sebastian@katallaxie.dev>
2024-04-03 20:29:44 +0000

    refactor(utils): rename foreach to map

--- components/utils/map.go
+++ components/utils/map.go
@@ -5,8 +5,8 @@ import htmx "github.com/zeiss/fiber-htmx"
 // Transformer is a function that transforms a node.
 type Transformer[T any] func(el T) htmx.Node
 
-// ForEach loops over the content.
-func ForEach[T any](transformer Transformer[T], elems ...T) htmx.Node {
+// Map is a function that maps a transformer to a list of htmx.Node.
+func Map[T any](transformer Transformer[T], elems ...T) htmx.Node {
 	nodes := []htmx.Node{}
 
 	for _, n := range elems {
