b10bcfa66aa05e001f364b4d4a1f9e1a664d6f4b
Sebastian Döll <sebastian@katallaxie.dev>
2024-05-23 13:13:48 +0000

    fix: add select options

--- components/forms/selects.go
+++ components/forms/selects.go
@@ -22,9 +22,170 @@ func Select(p SelectProps, children ...htmx.Node) htmx.Node {
 	)
 }
 
+// SelectBordered ...
+func SelectBordered(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":          true,
+				"select-bordered": true,
+				"w-full":          true,
+				"max-w-xs":        true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectGhost ...
+func SelectGhost(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":       true,
+				"select-ghost": true,
+				"w-full":       true,
+				"max-w-xs":     true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectPrimary ...
+func SelectPrimary(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":         true,
+				"select-primary": true,
+				"w-full":         true,
+				"max-w-xs":       true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectSecondary ...
+func SelectSecondary(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":           true,
+				"select-secondary": true,
+				"w-full":           true,
+				"max-w-xs":         true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectAccent ...
+func SelectAccent(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":        true,
+				"select-accent": true,
+				"w-full":        true,
+				"max-w-xs":      true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectInfo ...
+func SelectInfo(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":      true,
+				"select-info": true,
+				"w-full":      true,
+				"max-w-xs":    true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectSuccess ...
+func SelectSuccess(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":         true,
+				"select-success": true,
+				"w-full":         true,
+				"max-w-xs":       true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectWarning ...
+func SelectWarning(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":         true,
+				"select-warning": true,
+				"w-full":         true,
+				"max-w-xs":       true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectWarning ...
+func SelectDanger(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":        true,
+				"select-danger": true,
+				"w-full":        true,
+				"max-w-xs":      true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// SelectError ...
+func SelectError(p SelectProps, children ...htmx.Node) htmx.Node {
+	return htmx.Select(
+		htmx.Merge(
+			htmx.ClassNames{
+				"select":       true,
+				"select-error": true,
+				"w-full":       true,
+				"max-w-xs":     true,
+			},
+			p.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
 // OptionProps ...
 type OptionProps struct {
 	ClassNames htmx.ClassNames
+	Value      string
 	Selected   bool
 	Disabled   bool
 }
@@ -35,6 +196,7 @@ func Option(p OptionProps, children ...htmx.Node) htmx.Node {
 		htmx.Merge(p.ClassNames),
 		htmx.If(p.Selected, htmx.Selected()),
 		htmx.If(p.Disabled, htmx.Disabled()),
+		htmx.If(p.Value != "", htmx.Attribute("value", p.Value)),
 		htmx.Group(children...),
 	)
 }
