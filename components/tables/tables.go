38870cf1355018b48d21898aa9de588d218be1ee
Sebastian Döll <sebastian@katallaxie.dev>
2024-06-06 19:26:57 +0000

    feat(tables): add generic results type

--- components/tables/tables.go
+++ components/tables/tables.go
@@ -2,14 +2,100 @@ package tables
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/zeiss/fiber-htmx/components/buttons"
 	"github.com/zeiss/fiber-htmx/components/forms"
 	"github.com/zeiss/fiber-htmx/components/utils"
+	"gorm.io/gorm"
 
 	"github.com/gofiber/fiber/v2"
 	htmx "github.com/zeiss/fiber-htmx"
 )
+
+// Results is a struct that contains the results of a query
+type Results[R any] struct {
+	// Limit is the number of items to return.
+	Limit int `json:"limit" xml:"limit" form:"limit" query:"limit"`
+	// Offset is the number of items to skip.
+	Offset int `json:"offset" xml:"offset" form:"offset" query:"limit"`
+	// Search is the search term to filter the results.
+	Search string `json:"search,omitempty" xml:"search" form:"search" query:"limit"`
+	// Sort is the sorting order.
+	Sort string `json:"sort,omitempty" xml:"sort" form:"sort" query:"limit"`
+	// TotalRows is the total number of rows.
+	TotalRows int `json:"total_rows"`
+	// TotalPages is the total number of pages.
+	TotalPages int `json:"total_pages"`
+	// Rows is the items to return.
+	Rows []R `json:"rows" xml:"rows"`
+}
+
+// GetLimit returns the limit.
+func (p *Results[R]) GetLimit() int {
+	if p.Limit == 0 {
+		p.Limit = 10
+	}
+
+	return p.Limit
+}
+
+// GetOffset returns the page.
+func (p *Results[R]) GetOffset() int {
+	if p.Offset < 0 {
+		p.Offset = 0
+	}
+
+	return p.Offset
+}
+
+// GetSort returns the sort.
+func (p *Results[R]) GetSort() string {
+	if p.Sort == "" {
+		p.Sort = "desc"
+	}
+
+	return p.Sort
+}
+
+// GetRows returns the rows as pointers.
+func (p *Results[R]) GetRows() []*R {
+	rows := make([]*R, 0, len(p.Rows))
+	for _, row := range p.Rows {
+		rows = append(rows, &row)
+	}
+
+	return rows
+}
+
+// GetTotalRows returns the total rows.
+func (p *Results[R]) GetTotalRows() int {
+	return p.TotalRows
+}
+
+// GetTotalPages returns the total pages.
+func (p *Results[R]) GetTotalPages() int {
+	return p.TotalPages
+}
+
+// GetLen returns the length of the rows.
+func (p *Results[R]) GetLen() int {
+	return len(p.Rows)
+}
+
+// PaginatedResults returns a function that paginates the results.
+func PaginatedResults[R any](value interface{}, pagination *Results[R], db *gorm.DB) func(db *gorm.DB) *gorm.DB {
+	var totalRows int64
+	db.Model(value).Count(&totalRows).Where("deleted_at IS NULL")
+
+	pagination.TotalRows = int(totalRows)
+	totalPages := int(math.Ceil(float64(totalRows) / float64(pagination.Limit)))
+	pagination.TotalPages = totalPages
+
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Offset(pagination.GetOffset()).Limit(pagination.GetLimit())
+	}
+}
 
 // DefaultLimits is a list of default limits
 var DefaultLimits = []int{5, 10, 25, 50}
