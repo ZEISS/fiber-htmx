51bd7e170abeab22fd8f22f9d6f5013e67143ac5
Sebastian Döll <sebastian@katallaxie.dev>
2024-06-25 18:52:28 +0000

    fix: add props to text

--- components/typography/text.go
+++ components/typography/text.go
@@ -2,133 +2,150 @@ package typography
 
 import htmx "github.com/zeiss/fiber-htmx"
 
+// Props contains the properties for the text component.
+type Props struct {
+	// ClassNames contains the class names for the text component.
+	ClassNames htmx.ClassNames
+}
+
 // Error is a component that displays an error text.
-func Error(children ...htmx.Node) htmx.Node {
+func Error(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-error": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Info is a component that displays an info text.
-func Info(children ...htmx.Node) htmx.Node {
+func Info(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-info": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Success is a component that displays a success text.
-func Success(children ...htmx.Node) htmx.Node {
+func Success(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-success": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Warning is a component that displays a warning text.
-func Warning(children ...htmx.Node) htmx.Node {
+func Warning(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-warning": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Primary is a component that displays a primary text.
-func Primary(children ...htmx.Node) htmx.Node {
+func Primary(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-primary": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Secondary is a component that displays a secondary text.
-func Secondary(children ...htmx.Node) htmx.Node {
+func Secondary(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-secondary": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Accent is a component that displays an accent text.
-func Accent(children ...htmx.Node) htmx.Node {
+func Accent(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-accent": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Neutral is a component that displays a neutral text.
-func Neutral(children ...htmx.Node) htmx.Node {
+func Neutral(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-neutral": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Base100 is a component that displays a base100 text.
-func Base100(children ...htmx.Node) htmx.Node {
+func Base100(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-base-100": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Base200 is a component that displays a base200 text.
-func Base200(children ...htmx.Node) htmx.Node {
+func Base200(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-base-200": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
 }
 
 // Base300 is a component that displays a base300 text.
-func Base300(children ...htmx.Node) htmx.Node {
+func Base300(props Props, children ...htmx.Node) htmx.Node {
 	return htmx.Span(
 		htmx.Merge(
 			htmx.ClassNames{
 				"text-base-300": true,
 			},
+			props.ClassNames,
 		),
 		htmx.Group(children...),
 	)
