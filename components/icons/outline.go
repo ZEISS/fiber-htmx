fcc22017a2318031abc62f3b7dfb7a9b0be29f3d
Sebastian Döll <sebastian@katallaxie.dev>
2024-08-06 07:24:06 +0000

    feat: add arrow down icon

--- components/icons/outline.go
+++ components/icons/outline.go
@@ -660,3 +660,26 @@ func MinusCircleOutline(p IconProps) htmx.Node {
 		),
 	)
 }
+
+// ArrowDownOnSquareOutline ...
+func ArrowDownOnSquareOutline(p IconProps) htmx.Node {
+	return htmx.SVG(
+		htmx.Attribute("xmlns", "http://www.w3.org/2000/svg"),
+		htmx.Attribute("fill", "none"),
+		htmx.Attribute("viewBox", "0 0 24 24"),
+		htmx.Attribute("stroke-width", "1.5"),
+		htmx.Attribute("stroke", "currentColor"),
+		htmx.Merge(
+			htmx.ClassNames{
+				"w-6": true,
+				"h-6": true,
+			},
+			p.ClassNames,
+		),
+		htmx.Path(
+			htmx.Attribute("stroke-linecap", "round"),
+			htmx.Attribute("stroke-linejoin", "round"),
+			htmx.Attribute("d", "M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25"),
+		),
+	)
+}
