cde1e00d4f553241fe743d72d3a4af34c53472ff
Sebastian Döll <sebastian@katallaxie.dev>
2024-09-29 18:20:24 +0000

    chore: update release cycle

--- gen.go
+++ gen.go
@@ -2,6 +2,3 @@
 // +build generate
 
 package htmx
-
-//go:generate npx esbuild --bundle --minify --sourcemap --outfile=dist/fiber-htmx.min.js src/main.ts
-//go:generate npx esbuild --bundle --platform=neutral --packages=external --outfile=dist/fiber-htmx.esm.js src/main.ts
